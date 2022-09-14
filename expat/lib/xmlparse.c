9725b523e3334d14ce1e5463211cfec5c0700b87
Karen Arutyunov <karen@codesynthesis.com>
2022-09-14 16:16:35 +0300

    Patch CVE-2022-40674
    Upstream commit 4a32da87e931 (Ensure raw tagnames are safe exiting internalEntityParser).

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -5154,8 +5154,14 @@ internalEntityProcessor(XML_Parser parser,
   {
     parser->m_processor = contentProcessor;
     /* see externalEntityContentProcessor vs contentProcessor */
-    return doContent(parser, parser->m_parentParser ? 1 : 0, parser->m_encoding, s, end,
-                     nextPtr, (XML_Bool)!parser->m_parsingStatus.finalBuffer);
+    result = doContent(parser, parser->m_parentParser ? 1 : 0,
+                       parser->m_encoding, s, end, nextPtr,
+                       (XML_Bool)!parser->m_parsingStatus.finalBuffer);
+    if (result == XML_ERROR_NONE) {
+      if (! storeRawNames(parser))
+        return XML_ERROR_NO_MEMORY;
+    }
+    return result;
   }
 }
 
